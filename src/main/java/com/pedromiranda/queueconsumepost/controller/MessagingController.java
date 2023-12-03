45c1a66939d4ad1458f6a6009c50633e61617460
peterrsm <pedrorsmiranda@gmail.com>
2023-12-03 19:01:48 -0300

    + MessagingController refactor

--- src/main/java/com/pedromiranda/queueconsumepost/controller/MessagingController.java
+++ src/main/java/com/pedromiranda/queueconsumepost/controller/MessagingController.java
@@ -1,8 +1,10 @@
 package com.pedromiranda.queueconsumepost.controller;
 
+import com.pedromiranda.queueconsumepost.controller.interfaces.IMessagingController;
+import com.pedromiranda.queueconsumepost.message.Consumer;
+import com.pedromiranda.queueconsumepost.message.MessageToSend;
 import com.pedromiranda.queueconsumepost.message.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +13,25 @@ import java.util.concurrent.TimeoutException;
 
 @RestController
 @RequestMapping("/message")
-public class MessagingController {
+public class MessagingController implements IMessagingController {
     @Autowired
     Publisher pub = new Publisher();
 
-    @PostMapping
-    public void sendMessage() throws IOException, TimeoutException {
-        pub.publishMessage("Mensagem teste");
+    @Autowired
+    Consumer cons = new Consumer();
+
+    @Override
+    public void sendMessageToQueue(MessageToSend message) throws IOException, TimeoutException {
+        pub.publishMessage(message.getMessage());
     }
 
+    @Override
+    public void consumeQueue() throws IOException, TimeoutException {
+        cons.consumeQueue();
+    }
+
+    @Override
+    public void stopConsuming() throws IOException, TimeoutException {
+        cons.stopConsuming();
+    }
 }
