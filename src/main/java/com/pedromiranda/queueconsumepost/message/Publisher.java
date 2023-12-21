6af193b48121900a8b4944bfad38c33b90620611
peterrsm <pedrorsmiranda@gmail.com>
2023-12-21 19:50:59 -0300

    + Use of RabbitTemplate in Publisher class

--- src/main/java/com/pedromiranda/queueconsumepost/message/Publisher.java
+++ src/main/java/com/pedromiranda/queueconsumepost/message/Publisher.java
@@ -1,8 +1,8 @@
 package com.pedromiranda.queueconsumepost.message;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.pedromiranda.queueconsumepost.message.objects.MessageToSend;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -11,14 +11,11 @@ import java.util.concurrent.TimeoutException;
 @Component
 public class Publisher {
 
-    public void publishMessage(String msg) throws IOException, TimeoutException {
-        ConnectionFactory factory = new ConnectionFactory();
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
+    @Autowired
+    RabbitTemplate rabbitTemplate;
 
-        channel.basicPublish("", "simple-queue", null, msg.getBytes());
-
-        channel.close();
-        connection.close();
+    public void publishMessage(String queue, String msg) throws IOException, TimeoutException {
+        MessageToSend message = new MessageToSend(queue, msg);
+        rabbitTemplate.convertAndSend(message.getQueue_name(), message.getMessage());
     }
 }
