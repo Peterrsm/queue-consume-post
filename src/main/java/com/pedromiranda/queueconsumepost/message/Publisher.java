3129b9c0dd8c11ab5a77ae2df7513d3988f41a58
peterrsm <pedrorsmiranda@gmail.com>
2024-01-02 13:47:54 -0300

    + Publisher class adds: + publishMessageToDirectExchange method + publishMessageToFannoutExchange method + publishMessageToTopicExchange method + createByteMessage method
    + 'publishMessageToHeadersExchange' refactor

--- src/main/java/com/pedromiranda/queueconsumepost/message/Publisher.java
+++ src/main/java/com/pedromiranda/queueconsumepost/message/Publisher.java
@@ -1,6 +1,6 @@
 package com.pedromiranda.queueconsumepost.message;
 
-import com.pedromiranda.queueconsumepost.message.objects.MessageToSend;
+import com.pedromiranda.queueconsumepost.message.objects.MessageToSendToQueue;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -19,27 +19,60 @@ public class Publisher {
     @Autowired
     RabbitTemplate rabbitTemplate;
 
-    public void publishMessage(String queue, String msg) throws IOException, TimeoutException {
-        MessageToSend message = new MessageToSend(queue, msg);
+    public void publishMessageToQueue(String queue, String msg) throws IOException, TimeoutException {
+        MessageToSendToQueue message = new MessageToSendToQueue(queue, msg);
         rabbitTemplate.convertAndSend(message.getQueue_name(), message.getMessage());
     }
 
-    public void publishMessageToHeadersExchange(String queue_name, String text) throws IOException {
-        MessageToSend message = new MessageToSend(queue_name, text);
-
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ObjectOutput out = new ObjectOutputStream(baos);
-        out.writeObject(message.getMessage());
-        out.flush();
-        out.close();
-
-        byte[] byte_message = baos.toByteArray();
-        baos.close();
+    public void publishMessageToHeadersExchange(String text) throws IOException {
+        byte[] byte_message = createByteMessage(text);
 
         Message final_message = MessageBuilder.withBody(byte_message)
                 .setHeader("item-1", "second")
                 .build();
 
         rabbitTemplate.send("Headers-Exchange", "", final_message);
+    }
+
+    public void publishMessageToDirectExchange(String message, String routing_key) throws IOException {
+        byte[] byte_message = createByteMessage(message);
+
+        Message final_message = MessageBuilder.withBody(byte_message)
+                .setReceivedRoutingKey(routing_key)
+                .build();
+
+        rabbitTemplate.send("Direct-Exchange", routing_key, final_message);
+    }
+
+    public void publishMessageToFannoutExchange(String message) throws IOException {
+        byte[] byte_message = createByteMessage(message);
+
+        Message final_message = MessageBuilder.withBody(byte_message)
+                .build();
+
+        rabbitTemplate.send("Fannout-Exchange", "", final_message);
+    }
+
+    public void publishMessageToTopicExchange(String message, String routing_key) throws IOException {
+        byte[] byte_message = createByteMessage(message);
+
+        Message final_message = MessageBuilder.withBody(byte_message)
+                .setReceivedRoutingKey(routing_key)
+                .build();
+
+        rabbitTemplate.send("Topic-Exchange", routing_key, final_message);
+    }
+
+    private byte[] createByteMessage(String message) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutput out = new ObjectOutputStream(baos);
+        out.writeObject(message);
+        out.flush();
+        out.close();
+
+        byte[] byte_message = baos.toByteArray();
+        baos.close();
+
+        return byte_message;
     }
 }
