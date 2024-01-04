53c04054bc6ecbf847bb1d5fe3c13666a19c0887
peterrsm <pedrorsmiranda@gmail.com>
2024-01-04 13:28:30 -0300

    + MessagingController - adjust to return a ResponseEntity

--- src/main/java/com/pedromiranda/queueconsumepost/controller/MessagingController.java
+++ src/main/java/com/pedromiranda/queueconsumepost/controller/MessagingController.java
@@ -1,11 +1,11 @@
 package com.pedromiranda.queueconsumepost.controller;
 
 import com.pedromiranda.queueconsumepost.controller.interfaces.IMessagingController;
-import com.pedromiranda.queueconsumepost.message.Consumer;
-import com.pedromiranda.queueconsumepost.message.Publisher;
 import com.pedromiranda.queueconsumepost.message.objects.MessageToSendToQueue;
 import com.pedromiranda.queueconsumepost.message.objects.MessageToSendWithRoutingKey;
+import com.pedromiranda.queueconsumepost.service.MessagingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,33 +16,30 @@ import java.util.concurrent.TimeoutException;
 @RequestMapping("/message")
 public class MessagingController implements IMessagingController {
     @Autowired
-    Publisher pub = new Publisher();
-
-    @Autowired
-    Consumer cons = new Consumer();
+    MessagingService service;
 
     @Override
-    public void sendMessageToQueue(MessageToSendToQueue message) throws IOException, TimeoutException {
-        pub.publishMessageToQueue(message.getQueue_name(), message.getMessage());
+    public ResponseEntity<String> sendMessageToQueue(MessageToSendToQueue message) throws IOException, TimeoutException {
+        return service.publishMessageToQueue(message.getQueue_name(), message.getMessage());
     }
 
     @Override
-    public void sendMessageToDirectExchange(MessageToSendWithRoutingKey message) throws IOException, TimeoutException {
-        pub.publishMessageToDirectExchange(message.getMessage(), message.getRouting_key());
+    public ResponseEntity<String> sendMessageToDirectExchange(MessageToSendWithRoutingKey message) throws IOException {
+        return service.publishMessageToDirectExchange(message.getMessage(), message.getRouting_key());
     }
 
     @Override
-    public void sendMessageToFannoutExchange(MessageToSendToQueue message) throws IOException, TimeoutException {
-        pub.publishMessageToFannoutExchange(message.getMessage());
+    public ResponseEntity<String> sendMessageToFannoutExchange(MessageToSendToQueue message) throws IOException {
+        return service.publishMessageToFannoutExchange(message.getMessage());
     }
 
     @Override
-    public void sendMessageToTopicExchange(MessageToSendWithRoutingKey message) throws IOException, TimeoutException {
-        pub.publishMessageToTopicExchange(message.getMessage(), message.getRouting_key());
+    public ResponseEntity<String> sendMessageToTopicExchange(MessageToSendWithRoutingKey message) throws IOException {
+        return service.publishMessageToTopicExchange(message.getMessage(), message.getRouting_key());
     }
 
     @Override
-    public void sendMessageToHeaderExchange(MessageToSendToQueue message) throws IOException, TimeoutException {
-        pub.publishMessageToHeadersExchange(message.getMessage());
+    public ResponseEntity<String> sendMessageToHeaderExchange(MessageToSendToQueue message) throws IOException {
+        return service.publishMessageToHeadersExchange(message.getMessage());
     }
 }
